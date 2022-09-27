864c645c18b099e90d73ec77883bc953f762ddc2
matatoMatata94 <73385202+matatoMatata94@users.noreply.github.com>
2022-09-27 20:24:06 +0200

    Added some images

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,51 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        Expanded(
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset('images/dice1.png'),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset('images/dice2.png'),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset('images/dice3.png'),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset('images/dice4.png'),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset('images/dice5.png'),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset('images/dice6.png'),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
-dsfasdf
